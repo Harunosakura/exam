934befadc64391174f5a7c72b34e03af809ddbd3
Haruno Sakura <noon.ab@gmail.com>
2018-11-04 13:55:37 +0200

    bad performance at datesFrequency variable collection

--- Challenges/src/challenge/booking/woman1/HighlightLandmark.java
+++ Challenges/src/challenge/booking/woman1/HighlightLandmark.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+https://www.hackerrank.com/contests/booking-womenintech-2017/challenges/highlight-landmarks
  */
 package challenge.booking.woman1;
 
